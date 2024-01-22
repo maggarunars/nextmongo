49de860aa919a3b2fc0a672a0da55f91a3d130d6
maggarunars <margretrunars@gmail.com>
2024-01-22 09:27:54 +0000

    fixed link

--- src/app/back/page.tsx
+++ src/app/back/page.tsx
@@ -6,7 +6,7 @@ type Greeting = {
 }
   
 export default async function Back() {
-  const baseUrl = "nextmongo-1ql6hedud-margrets-projects.vercel.app"
+  const baseUrl = "https://nextmongo-j56p9yzwd-margrets-projects.vercel.app/"
   const response = await fetch(`${baseUrl}/api`)
   const greetings: Greeting[] = await response.json()
   return (
